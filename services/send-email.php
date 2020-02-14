bc71a2f0f2107c689b7cc2d0c13a1ef859a877d1
Ramon C <ramon.pires@apllos.com>
2020-02-13 23:19:12 -0300

    modify send-email.php

--- services/send-email.php
+++ services/send-email.php
@@ -1,2 +1,31 @@
-<?php
 
+<?php	
+    require_once("../lib/phpmailer/class.phpmailer.php");
+    $body = "";
+    $subject = "";
+    $mailDestino = "";
+    $name = "";
+
+	$mail = new PHPMailer();
+	$mail->IsSMTP(); 
+	$mail->CharSet = 'UTF-8';
+	$mail->True;
+	$mail->Host = "smtp.gmail.com";
+	$mail->Port = 465; 
+	$mail->SMTPAuth = true; 
+	$mail->Username = ""; 
+	$mail->Password = ""; 
+	$mail->From = ""; 
+	$mail->FromName = "" ; 
+	$mail->AddAddress($mailDestino, $name); 
+	$mail->WordWrap = 50; 
+	$mail->IsHTML = true ; 
+	$mail->Subject = $subject ; 
+	$mail->Body = '<br/>' . $body . '<br/>' ; 
+	$mail->AltBody = "$mensagem" ; 
+
+	if(!$mail->Send()) 
+	{	
+		echo "Error";
+	}	
+?>
